aedef5805a3832ef607047b044ab44b8599d4394
booring37 <luvamaandongol@gmail.com>
2026-02-13 18:18:44 +0545

    Add initial HTML structure through JS and implement css of grocery list.

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+const app = document.getElementById("app");
+
+function render() {
+  app.innerHTML = `
+    <div class="grocery-container">
+      <h1 class="title">Grocery List</h1>
+      
+      <form class="grocery-form">
+        <input 
+          type="text" 
+          id="grocery-input" 
+          class="grocery-input" 
+          placeholder="e.g., Milk"
+          required
+        />
+        <button type="submit" class="submit-btn">
+          Add Item
+        </button>
+      </form>
+      
+      <div id="item-list" class="grocery-list">
+      </div>
+    </div>
+  `;
+}
+
+render();
